01f686b8172b82e27ed6d1ab20868f36010e3a91
RelativeForce <joshiceddy@hotmail.com>
2016-12-03 14:51:35 +0000

    Added File (Josh)
    Added Entity.java

---
+++ LocalHackPlatformer/src/entities/Entity.java
@@ -0,0 +1,28 @@
+package entities;
+
+
+
+public class Entity {
+
+	private int x;
+	private int y;
+	// private Image image; 
+	// private HitBox hitBox;
+	
+	public int getX(){
+		return x;
+	}
+	public int getY(){
+		return y;
+	}
+/* 
+	public Image getImage(){
+		return image;
+	}
+	
+	public HitBox getHitBox(){
+		return hitBox;
+	}
+*/
+	
+}
