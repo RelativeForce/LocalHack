04c9f337aaff56fe4de2aad81ffeef39d199527b
RelativeForce <joshiceddy@hotmail.com>
2016-12-03 13:28:32 +0000

    Edit File (Josh)
    edited Main.java

--- LocalHackPlatformer/src/Environment/Main.java
+++ LocalHackPlatformer/src/Environment/Main.java
@@ -4,11 +4,27 @@ import Logic.*;
 import Graphics.*;
 
 public class Main {
+	
+	private static Thread logicThread;
 
 	public static void main(String[] args) {
 		
 		
-
+		
+	}
+	
+	private static void initaliseLogicThread(){
+		
+		logicThread = new Thread(){
+			
+			@Override
+			public void run(){
+				
+				
+				
+			}
+		};
+		
 	}
 
 }
