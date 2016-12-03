8d8c6cd6622399564c17ab8d9ee0b2144553dbc7
RelativeForce <joshiceddy@hotmail.com>
2016-12-03 15:00:22 +0000

    Files Modified (Josh)
    Added Rectangle.java

--- LocalHackPlatformer/src/entities/Entity.java
+++ LocalHackPlatformer/src/entities/Entity.java
@@ -1,28 +1,35 @@
 package entities;
 
-
-
 public class Entity {
 
 	private int x;
 	private int y;
-	// private Image image; 
+	// private Image image;
 	// private HitBox hitBox;
-	
-	public int getX(){
+
+	public void setX(int x) {
+		if (x >= 0) {
+			this.x = x;
+		}
+	}
+
+	public void setY(int y) {
+		if (y >= 0) {
+			this.y = y;
+		}
+	}
+
+	public int getX() {
 		return x;
 	}
-	public int getY(){
+
+	public int getY() {
 		return y;
 	}
-/* 
-	public Image getImage(){
-		return image;
-	}
-	
-	public HitBox getHitBox(){
-		return hitBox;
-	}
-*/
-	
+	/*
+	 * public Image getImage(){ return image; }
+	 * 
+	 * public HitBox getHitBox(){ return hitBox; }
+	 */
+
 }
---
+++ LocalHackPlatformer/src/entities/Rectangle.java
@@ -0,0 +1,20 @@
+package entities;
+
+public class Rectangle extends Entity {
+
+	public Rectangle(int x, int y, int height, int width) {
+
+		setX(x);
+		setY(y);
+
+		// Create Image
+		// Create HitBox
+
+	}
+
+	public void changeSize(int height, int width) {
+		
+		// redefine image
+		
+	}
+}
