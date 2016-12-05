62565067bf89ff573b61e42e1e2fcb820f96926c
RelativeForce <joshiceddy@hotmail.com>
2016-12-05 10:15:24 +0000

    Edited file (Josh)

--- LocalHackPlatformer/src/Environment/Constants.java
+++ LocalHackPlatformer/src/Environment/Constants.java
@@ -4,6 +4,6 @@ public class Constants {
 
 	public static final int GRAVITY = 1;
 	public static final int FRICTION = 5;
-	public static final int JUMP_HEIGHT = 20;
+	public static final int JUMP_HEIGHT = 25;
 	
 }
