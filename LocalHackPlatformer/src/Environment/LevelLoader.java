919e363d4c1666128e33c2e40ce66796c94a4130
RelativeForce <joshiceddy@hotmail.com>
2016-12-05 13:11:16 +0000

    Edited File (Josh)

--- LocalHackPlatformer/src/Environment/LevelLoader.java
+++ LocalHackPlatformer/src/Environment/LevelLoader.java
@@ -1,8 +1,34 @@
 package Environment;
 
+import java.util.Scanner;
+import java.io.File;
+import java.util.ArrayList;
+
+/**
+ * @author Joshua_Eddy
+ * 
+ */
+
 public class LevelLoader {
 
-	
-	
-	
+	private ArrayList<Scanner> scnr;
+
+	public LevelLoader(String directory) {
+
+		try {
+			scnr = new ArrayList<Scanner>();
+			File folder = new File(directory);
+			File[] levels = folder.listFiles();
+
+			for(File level : levels){
+				if(level.getPath().contains("LH_Level")){
+					scnr.add(new Scanner(level.getPath()));
+				}
+			}
+			
+		} catch (Exception e) {
+			System.out.println("Failed to load level");
+		}
+	}
+
 }
