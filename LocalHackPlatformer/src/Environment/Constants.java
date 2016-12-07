4e8392652e4643da4ce5d1d5c0661009fe8f3ea5
RelativeForce <joshiceddy@hotmail.com>
2016-12-07 18:51:36 +0000

    Edited Constants

--- LocalHackPlatformer/src/Environment/Constants.java
+++ LocalHackPlatformer/src/Environment/Constants.java
@@ -8,6 +8,6 @@ public class Constants {
 	public static final int WINDOW_WIDTH = 1000;
 	public static final int WINDOW_HEIGHT = 500;
 	public static final int WINDOW_PADDING = 100;
-	public static final int MOVE_DISTANCE = 1;
+	public static final int MOVE_DISTANCE = 3;
 	
 }
