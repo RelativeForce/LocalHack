b86208eb898507a5fa76232213755c242e5ccfee
JohnBeeBlue <john.berg.96bits@gmail.com>
2017-01-12 14:56:22 +0000

    John
    Added SpriteSheetReader

--- LocalHackPlatformer/src/graphics/SpriteSheetReader.java
+++ LocalHackPlatformer/src/graphics/SpriteSheetReader.java
@@ -1,8 +1,19 @@
 package graphics;
 
+import java.io.File;
+import java.io.FileReader;
+
 public class SpriteSheetReader {
 	
+	private int width;
+	private int height;
+	private final File file;
 	
-	
+	public SpriteSheetReader(String path, int width, int height) {
+		
+		this.width = width;
+		this.height = height;
+		file = new File(path);
+	}
 	
 }
