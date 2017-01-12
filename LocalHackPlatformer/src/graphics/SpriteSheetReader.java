027fd378b5aab271df0535c3aa6f9f0c66096090
JohnBeeBlue <john.berg.96bits@gmail.com>
2017-01-12 16:57:20 +0000

    Changes made to SpriteSheetReader John

--- LocalHackPlatformer/src/graphics/SpriteSheetReader.java
+++ LocalHackPlatformer/src/graphics/SpriteSheetReader.java
@@ -1,19 +1,47 @@
 package graphics;
 
 import java.io.File;
-import java.io.FileReader;
+import java.io.IOException;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+import graphics.objects.SpriteFrame;
 
-public class SpriteSheetReader {
+public final class SpriteSheetReader {
 	
-	private int width;
-	private int height;
 	private final File file;
+	private final BufferedImage image;
 	
-	public SpriteSheetReader(String path, int width, int height) {
+	private SpriteSheetReader(final String path) {
 		
-		this.width = width;
-		this.height = height;
 		file = new File(path);
+		image = getImage();
 	}
-	
+	private BufferedImage getImage(){
+		
+		try{
+			
+			return ImageIO.read(file);
+			
+		}catch(IOException e){
+			
+			e.printStackTrace();
+			return null;
+		}
+	}
+	/**
+	 * Read a spritesheet from a given path where each sprite is a fixed width and height.
+	 * 
+	 * @param path The path to the spritesheet.
+	 * @param width Sprite width.
+	 * @param height Sprite height.
+	 * @return The sprites contained within the target spritesheet.
+	 * @See SpriteFrame
+	 */
+	public static final SpriteFrame[] getSprites(final String path, final int width, final int height){
+		
+		SpriteSheetReader spriteSheet = new SpriteSheetReader(path);
+		
+		
+		return null;
+	}
 }
