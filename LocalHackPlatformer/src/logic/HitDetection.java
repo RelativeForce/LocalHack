da10041aa7a1d48733a0d85d392833f13f27b244
RelativeForce <joshiceddy@hotmail.com>
2017-01-23 14:57:44 +0000

    General Chnages Changes to underling functions that have no effect on current implementation.

--- LocalHackPlatformer/src/logic/HitDetection.java
+++ LocalHackPlatformer/src/logic/HitDetection.java
@@ -4,28 +4,36 @@ import entities.Entity;
 
 /**
  * Contains the Logic for detecting hits between Entities.
+ * 
  * @author Joshua_Eddy
  *
  */
 public class HitDetection {
 
 	/**
-	 * Checks if entity1 will collide with entity2 before it reaches the location of nextEntity1.
-	 * @param entity1 The initial position of the first Entity.
-	 * @param nextEntity1 The final position of the first Entity.
-	 * @param entity2 The current position of the second Entity.
+	 * Checks if entity1 will collide with entity2 before it reaches the
+	 * location of nextEntity1.
+	 * 
+	 * @param entity1
+	 *            The initial position of the first Entity.
+	 * @param nextEntity1
+	 *            The final position of the first Entity.
+	 * @param entity2
+	 *            The current position of the second Entity.
 	 * @return Whether entity1 has collided with entity2.
 	 */
-	public static boolean detectHit(Entity entity1, Entity nextEntity1, Entity entity2) {
+	public static boolean detectHit(Entity entity1, Entity nextEntity1,
+			Entity entity2) {
 
-		if (checkX(entity1, nextEntity1, entity2) && checkY(entity1, nextEntity1, entity2)) {
+		if (checkX(entity1, nextEntity1.getX(), entity2)
+				&& checkY(entity1, nextEntity1.getY(), entity2)) {
 			return true;
 		}
 
 		return false;
 	}
 
-	private static boolean checkX(Entity entity1, Entity nextEntity1, Entity entity2) {
+	private static boolean checkX(Entity entity1, int nextX, Entity entity2) {
 
 		Integer[][] obj1XPerimeter = entity1.getHitBox().getXPerimeter();
 		Integer[][] obj2XPerimeter = entity2.getHitBox().getXPerimeter();
@@ -33,19 +41,21 @@ public class HitDetection {
 		int initalX;
 		int finalX;
 
-		if (entity1.getX() > nextEntity1.getX()) {
-			initalX = nextEntity1.getX();
+		if (entity1.getX() > nextX) {
+			initalX = nextX;
 			finalX = entity1.getX();
 		} else {
 			initalX = entity1.getX();
-			finalX = nextEntity1.getX();
+			finalX = nextX;
 		}
 
 		int obj2x = entity2.getX();
 
 		for (int x = initalX; x <= finalX; x++) {
-			for (int rowObj1 = 0; rowObj1 < entity1.getGraphicalObject().getHeight(); rowObj1++) {
-				for (int rowObj2 = 0; rowObj2 < entity2.getGraphicalObject().getHeight(); rowObj2++) {
+			for (int rowObj1 = 0; rowObj1 < entity1.getGraphicalObject()
+					.getHeight(); rowObj1++) {
+				for (int rowObj2 = 0; rowObj2 < entity2.getGraphicalObject()
+						.getHeight(); rowObj2++) {
 					boolean condition1 = (x + obj1XPerimeter[rowObj1][0]) <= (obj2x + obj2XPerimeter[rowObj2][1]);
 					boolean condition2 = (x + obj1XPerimeter[rowObj1][1]) >= (obj2x + obj2XPerimeter[rowObj2][0]);
 					if (condition1 && condition2) {
@@ -58,26 +68,28 @@ public class HitDetection {
 		return false;
 	}
 
-	private static boolean checkY(Entity entity1, Entity nextEntity1, Entity entity2) {
+	private static boolean checkY(Entity entity1, int nextY, Entity entity2) {
 
 		Integer[][] obj1YPerimeter = entity1.getHitBox().getYPerimeter();
 		Integer[][] obj2YPerimeter = entity2.getHitBox().getYPerimeter();
 
 		int initalY;
 		int finalY;
-		if (entity1.getY() > nextEntity1.getY()) {
-			initalY = nextEntity1.getY();
+		if (entity1.getY() > nextY) {
+			initalY = nextY;
 			finalY = entity1.getY();
 		} else {
 			initalY = entity1.getY();
-			finalY = nextEntity1.getY();
+			finalY = nextY;
 		}
 
 		int obj2Y = entity2.getY();
 
 		for (int y = initalY; y <= finalY; y++) {
-			for (int colObj1 = 0; colObj1 < entity1.getGraphicalObject().getWidth(); colObj1++) {
-				for (int colObj2 = 0; colObj2 < entity2.getGraphicalObject().getWidth(); colObj2++) {
+			for (int colObj1 = 0; colObj1 < entity1.getGraphicalObject()
+					.getWidth(); colObj1++) {
+				for (int colObj2 = 0; colObj2 < entity2.getGraphicalObject()
+						.getWidth(); colObj2++) {
 					boolean condition1 = (y + obj1YPerimeter[colObj1][0]) <= (obj2Y + obj2YPerimeter[colObj2][1]);
 					boolean condition2 = (y + obj1YPerimeter[colObj1][1]) >= (obj2Y + obj2YPerimeter[colObj2][0]);
 
@@ -91,4 +103,32 @@ public class HitDetection {
 		return false;
 	}
 
+	/**
+	 * Retrieves the element from a collection of type <code>Entity</code> that
+	 * a specified <code>Entity</code> will collide with on its path to a final
+	 * position.
+	 * 
+	 * If entity does not collide with any elements in entities the function
+	 * will return </code>null</code>.
+	 * 
+	 * Otherwise the function will return the <strong>first</strong> element in
+	 * the collection that the specified <code>Entity</code> collides with.
+	 * 
+	 * @param entity
+	 *            A <code>Entity</code> that will collide with an element from
+	 *            entities .
+	 * @param finalPosition
+	 *            The <code>Point</code> that denotes the final position that
+	 *            entity will have.
+	 * @param entities
+	 *            The collection of entities that entity may collide with.
+	 * @return The <strong>first</strong> element from a collection of type
+	 *         <code>Entity</code> that the specified specified
+	 *         <code>Entity</code> collided with, if any.
+	 */
+	public static Entity getObstruction(Entity entity, Point finalPosition,
+			Entity[] entities) {
+
+		return null;
+	}
 }
