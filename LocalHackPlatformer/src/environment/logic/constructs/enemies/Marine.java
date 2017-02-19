93ca5f0a890d23832416070286f3674aa8e8f0da
RelativeForce <joshiceddy@hotmail.com>
2017-02-19 14:52:03 +0000

    Maintenance
    1.[Fix] Imported Point.java and added getMove() as it is required to be
    an enemy.

--- LocalHackPlatformer/src/environment/logic/constructs/enemies/Marine.java
+++ LocalHackPlatformer/src/environment/logic/constructs/enemies/Marine.java
@@ -1,13 +1,13 @@
 package environment.logic.constructs.enemies;
 
-import java.io.File;
+import environment.logic.Point;
 
 /**
  * A marine is a second edition grunt featuring the basic concepts of a grunt
  * but implementing a range attack and slightly altered movement pattern.
  * 
  *
- * @author Wightman
+ * @author David_Wightman
  *
  */
 
@@ -16,11 +16,18 @@ public class Marine extends Grunt implements Enemy {
 	private int Ammunition;
 	private int level;
 
-	public Marine(int ammocount, int level,Point initial) {
+	public Marine(int ammocount, int level, Point initial) {
 		super(initial);
 		Ammunition = ammocount;
 		this.level = level;
 
+	}
+	
+	@Override
+	public void getMove(){
+		
+		
+		
 	}
 
 	public void Fire() {
